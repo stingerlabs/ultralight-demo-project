350976e94887f2cca27f32a4e1ba52ae1e9eec62
shivghai <8965168+shivghai@users.noreply.github.com>
2024-09-13 10:46:32 -0400

    update test reporting

--- next/e2e/pagesTest.spec.ts
+++ next/e2e/pagesTest.spec.ts
@@ -1,12 +1,12 @@
 import { test, expect } from '@playwright/test';
 
-test('VER-4 has "Hello World!" on homepage', async ({ page }) => {
+test('VER-2 has "Hello World!" on homepage', async ({ page }) => {
   await page.goto('http://localhost:3000/');
 
   await expect(page.getByText('Hello world!')).toBeVisible()
 });
 
-test('VER-5 clicking on Test! link opens test page', async ({ page }) => {
+test('clicking on Test! link opens test page', async ({ page }) => {
   await page.goto('http://localhost:3000/');
 
   await page.getByRole('link', { name: 'Test!' }).click();
